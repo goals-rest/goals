645525b83eec7970459237ee08ee6d80f18a21f5
Karine Vieira <kv.vieira1@gmail.com>
2025-01-16 09:01:11 -0300

    remove validação de data de término a nivel do banco

--- db/migrate/20250112183014_create_goals.rb
+++ db/migrate/20250112183014_create_goals.rb
@@ -4,7 +4,7 @@ class CreateGoals < ActiveRecord::Migration[8.1]
       t.string :title, null: false
       t.string :description
       t.datetime :start_date, null: false
-      t.datetime :end_date, null: false
+      t.datetime :end_date
       t.decimal :current, precision: 10, scale: 2, null: false, default: 0
       t.decimal :target, precision: 10, scale: 2, null: false, default: 0
       t.integer :status, null: false, default: 0
