5a41f685798b68eeb94462b377a4fd6e5d289be3
Lucas Sousa <ls442336@gmail.com>
2025-01-18 13:22:13 -0300

    feat: Add a modal component

--- app/components/ui/modal/component.rb
+++ app/components/ui/modal/component.rb
@@ -1,6 +1,35 @@
 module UI
   module Modal
     class Component < ApplicationComponent
+      renders_one :trigger, ApplicationComponent
+
+      style do
+        base do
+          %w[
+            hidden rounded-xl mx-auto shadow-xl max-w-lg bg-white m-1
+            origin-bottom text-center dark:bg-zinc-700 dark:text-zinc-200
+          ]
+        end
+      end
+
+      def initialize(**user_attrs)
+        super(**user_attrs)
+      end
+
+      def default_attrs
+        {
+          class: style,
+          data: {
+            railsui_modal_target: "content",
+            transition_enter_active: "transition ease-out duration-300",
+            transition_enter_from: "transform opacity-0 scale-95",
+            transition_enter_to: "transform opacity-100 scale-100",
+            transition_leave_active: "transition ease-in duration-300",
+            transition_leave_from: "transform opacity-100 scale-100",
+            transition_leave_to: "transform opacity-0 scale-95"
+          }
+        }
+      end
     end
   end
 end
--- app/components/ui/modal/preview.rb
+++ app/components/ui/modal/preview.rb
@@ -3,6 +3,11 @@ module UI
     class Preview < ViewComponent::Preview
       def playground
         render UI::Modal::Component.new do |modal|
+          modal.with_trigger do
+            modal.render UI::Button::Component.new.with_content("Open Modal")
+          end
+
+          "The modal content goes here"
         end
       end
     end
