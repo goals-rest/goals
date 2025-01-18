325022d9a75658a39a3695e67b1a399d63e0d165
Lucas Sousa <ls442336@gmail.com>
2025-01-18 15:10:17 -0300

    refactor: Move the card playground preview to a separate folder

--- app/components/ui/modal/preview.rb
+++ app/components/ui/modal/preview.rb
@@ -2,13 +2,7 @@ module UI
   module Modal
     class Preview < ViewComponent::Preview
       def playground
-        render UI::Modal::Component.new do |modal|
-          modal.with_trigger do
-            modal.render UI::Button::Component.new.with_content("Open Modal")
-          end
-
-          "The modal content goes here"
-        end
+        render_with_template(template: "ui/modal/previews/playground")
       end
     end
   end
