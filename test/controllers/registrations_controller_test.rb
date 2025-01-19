6cd9a24ac1d3047f933f464e4cfc44444c9e71dd
Lucas Sousa <ls442336@gmail.com>
2025-01-19 15:26:35 -0300

    feat: Add user registration

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -5,4 +5,37 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     get new_registration_url
     assert_response :success
   end
+
+  test "when params are valid redirects to root_url" do
+    params = {
+      user: attributes_for(:user, password: "123456", password_confirmation: "123456")
+    }
+
+    post registrations_url, params: params
+
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "when params are valid creates a new user" do
+    params = {
+      user: attributes_for(:user, password: "123456", password_confirmation: "123456")
+    }
+
+    assert_difference "User.count", +1 do
+      post registrations_url, params: params
+    end
+
+    assert_response :redirect
+  end
+
+  test "when params are invalid returns a unprocessable_entity status" do
+    params = {
+      user: attributes_for(:user, password_confirmation: "123")
+    }
+
+    post registrations_url, params: params
+
+    assert_response :unprocessable_entity
+  end
 end
