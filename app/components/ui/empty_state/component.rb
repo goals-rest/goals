24868b0d26e9e4ba8bf81b66aee97b3132e33f8e
Lucas Sousa <ls442336@gmail.com>
2025-01-19 22:06:39 -0300

    feat: Add empty states for goals not found and no goals

--- app/components/ui/empty_state/component.rb
+++ app/components/ui/empty_state/component.rb
@@ -9,18 +9,35 @@ module UI
       style do
         base do
           %w[
-            text-center flex flex-col items-center justify-center space-y-2 py-6 md:py-16
+            text-center flex flex-col items-center justify-center space-y-2
           ]
         end
+
+        variants {
+          narrow {
+            yes { "py-6 md:py-8" }
+            no { "py-6 md:py-16" }
+          }
+        }
+
+        defaults { { narrow: false } }
       end
 
-      def initialize(**user_attrs)
+      attr_reader :narrow
+
+      def initialize(narrow: false, **user_attrs)
+        @narrow = narrow
+
         super(**user_attrs)
+      end
+
+      def narrow?
+        narrow
       end
 
       def default_attrs
         {
-          class: style
+          class: style(narrow: narrow?)
         }
       end
     end
