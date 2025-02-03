a4bab65400b2f318c4059e2d937712e3f4f0171d
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2025-02-03 18:10:16 -0300

    refactor: Remove context and switch should for test (#86)

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,10 +1,8 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  context "validations" do
-    should validate_presence_of(:title)
-    should validate_presence_of(:body)
+  should validate_presence_of(:title)
+  should validate_presence_of(:body)
 
-    should validate_length_of(:title).is_at_most(60)
-  end
+  should validate_length_of(:title).is_at_most(60)
 end
