a0c3dd70b74c93a2458a2c88e6b603aa5ac09aab
Lucas Sousa <ls442336@gmail.com>
2025-02-07 21:22:20 -0300

    feat: Move icon classes to component class

--- app/components/common/like_button/component.rb
+++ app/components/common/like_button/component.rb
@@ -1,5 +1,13 @@
 module LikeButton
   class Component < ApplicationComponent
+    style :icon do
+      variants {
+        liked {
+          yes { "text-primary-600 dark:text-primary-500" }
+        }
+      }
+    end
+
     def initialize(liked: false, **user_attrs)
       @liked = liked
 
@@ -17,6 +25,20 @@ module LikeButton
         color: :transparent,
         size: :sm
       }
+    end
+
+    private
+    def icon_attrs
+      {
+        variant: icon_variant,
+        class: style(:icon, liked: liked?)
+      }
+    end
+
+    def icon_variant
+      return :solid if liked?
+
+      :outline
     end
   end
 end
