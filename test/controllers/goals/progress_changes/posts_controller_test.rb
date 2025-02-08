342702c4c0c097c07a68364a726dd3b8ffa4a3b0
Lucas Sousa <ls442336@gmail.com>
2025-02-08 09:42:18 -0300

    feat: Add image upload to progress update posts

--- test/controllers/goals/progress_changes/posts_controller_test.rb
+++ test/controllers/goals/progress_changes/posts_controller_test.rb
@@ -47,4 +47,30 @@ class Goals::ProgressChanges::PostsControllerTest < ActionDispatch::IntegrationT
 
     assert_response :unprocessable_entity
   end
+
+  test "can create post with images" do
+    user = create(:user)
+    goal = create(:goal, user:)
+    progress_change = create(:goal_progress_change, goal:)
+    params = {
+      post: attributes_for(
+        :post,
+        images: [
+          file_fixture_upload("avatar_placeholder.png", "image/png")
+        ]
+      )
+    }
+
+    sign_in user
+
+    assert_difference("Post.count") do
+      post goal_goal_progress_change_posts_path(goal_id: goal.id, goal_progress_change_id: progress_change.id), params: params
+    end
+
+    post = Post.last
+
+    assert_response :redirect
+    assert_redirected_to home_url
+    assert_equal 1, post.images.count
+  end
 end
