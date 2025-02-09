bda89bcc9ea0c2daf115860b64d41c8621fe2d03
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-02-09 11:01:12 -0300

    feat: Add see more goals list (#111)

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < DashboardController
   def show
     @user = Current.user
-    @goals = @user.goals
+    @goals = @user.goals.pending.limit(5).to_a.sort_by(&:progress).reverse
   end
 end
