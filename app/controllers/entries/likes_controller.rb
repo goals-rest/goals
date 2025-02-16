4ba2204aa8a1625151986da2ec2cb6c27d6e55ac
Karine Vieira <kv.vieira1@gmail.com>
2025-02-16 16:30:04 -0300

    feat: Add load more button

--- app/controllers/entries/likes_controller.rb
+++ app/controllers/entries/likes_controller.rb
@@ -7,7 +7,7 @@ module Entries
     before_action :set_like, only: :destroy
 
     def index
-      @likes = @entry.likes
+      @pagy, @likes = pagy(@entry.likes.order(created_at: :desc), limit: 1)
     end
 
     def create
