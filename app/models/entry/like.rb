23e9522ec0cdac4c33ff504802f715e06adc006c
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-02-22 18:36:12 -0300

    fix: Update the entryable (#142)
    * fix: Update the entryable
    
    * config: Remove unnecessary include

--- app/models/entry/like.rb
+++ app/models/entry/like.rb
@@ -3,4 +3,12 @@ class Entry::Like < ApplicationRecord
   belongs_to :user
 
   validates :user, uniqueness: { scope: :entry }
+
+  after_save :touch_entryable
+  after_destroy :touch_entryable
+
+  private
+  def touch_entryable
+    entry.entryable.touch
+  end
 end
--- test/models/entry/like_test.rb
+++ test/models/entry/like_test.rb
@@ -15,4 +15,26 @@ class Entry::LikeTest < ActiveSupport::TestCase
     assert_not like.valid?
     assert_includes like.errors[:user], I18n.t("errors.messages.taken")
   end
+
+  test "after save touches the entryable" do
+    entry = create(:entry, :post)
+    user = create(:user)
+
+    like = create(:entry_like, entry:, user:)
+
+    assert_changes -> { entry.entryable.updated_at } do
+      like.save!
+    end
+  end
+
+  test "after destroy touches the entryable" do
+    entry = create(:entry, :post)
+    user = create(:user)
+
+    like = create(:entry_like, entry:, user:)
+
+    assert_changes -> { entry.entryable.updated_at } do
+      like.destroy
+    end
+  end
 end
