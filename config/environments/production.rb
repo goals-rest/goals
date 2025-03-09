ac537e2db27a79afe63ba9246c7188cd139a18ff
Lucas Sousa <ls442336@gmail.com>
2025-03-09 14:58:52 -0300

    config: Add config for yjit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Config yjit usage
+  config.yjit = ENV.fetch("YJIT_ENABLED", "false") == "true"
 end
