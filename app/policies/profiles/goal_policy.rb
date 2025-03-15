2f8928e6b14ceafae6844b2ae9cd9d4aff2614c2
Lucas Sousa <ls442336@gmail.com>
2025-03-15 14:56:07 -0300

    feat: Add goal show page

--- app/policies/profiles/goal_policy.rb
+++ app/policies/profiles/goal_policy.rb
@@ -1,10 +1,21 @@
 module Profiles
   class GoalPolicy < ProfilesPolicy
+    def show?
+      return true if owner?
+
+      record.public_goal? && (profile_owner&.public_profile? || follows_profile_owner?)
+    end
+
     relation_scope do |relation|
       next relation.where(user: profile_owner) if user&.id == profile_owner.id
 
       relation.where(user: profile_owner)
               .where(visibility: :public)
+    end
+
+    private
+    def owner?
+      record.user_id == user&.id
     end
   end
 end
