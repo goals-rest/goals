323902206c70dde5ae84793dd6b55f4d39eb9c07
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-03-16 17:57:54 -0300

    feat: Add goal show page (#189)

--- app/policies/profiles/goal_policy.rb
+++ app/policies/profiles/goal_policy.rb
@@ -1,10 +1,25 @@
 module Profiles
   class GoalPolicy < ProfilesPolicy
+    pre_check :allow_owner
+
+    def show?
+      record.public_goal? && (profile_owner&.public_profile? || follows_profile_owner?)
+    end
+
     relation_scope do |relation|
       next relation.where(user: profile_owner) if user&.id == profile_owner.id
 
       relation.where(user: profile_owner)
               .where(visibility: :public)
+    end
+
+    private
+    def owner?
+      record.user_id == user&.id
+    end
+
+    def allow_owner
+      owner?
     end
   end
 end
