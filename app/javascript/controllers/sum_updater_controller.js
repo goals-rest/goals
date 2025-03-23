1fa8e24c452c5eeb3ece2d314db41ef319397988
Lucas Sousa <ls442336@gmail.com>
2025-03-23 13:27:08 -0300

    feat: Add counter input to progress update

--- app/javascript/controllers/sum_updater_controller.js
+++ app/javascript/controllers/sum_updater_controller.js
@@ -1,19 +1,23 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["output", "fieldOutput"];
+  static targets = ['input', 'output', 'fieldOutput']
   static values = {
     base: {
       type: Number,
       default: 0,
     },
-  };
+  }
 
-  update({ target }) {
-    const valueChange = parseFloat(target.value) || 0;
-    const result = this.baseValue + valueChange;
+  update() {
+    if (!this.hasInputTarget) {
+      return
+    }
 
-    this.outputTargets.forEach((output) => (output.innerText = result));
-    this.fieldOutputTargets.forEach((output) => (output.value = result));
+    const valueChange = parseFloat(this.inputTarget.value) || 0
+    const result = this.baseValue + valueChange
+
+    this.outputTargets.forEach((output) => (output.innerText = result))
+    this.fieldOutputTargets.forEach((output) => (output.value = result))
   }
 }
