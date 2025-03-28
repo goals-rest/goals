1927387ff6fbd73f4a936d14878ad56cf43ff14c
Lucas Sousa <ls442336@gmail.com>
2025-03-28 18:43:32 -0300

    Adds the mention component

--- test/models/mention_test.rb
+++ test/models/mention_test.rb
@@ -4,4 +4,11 @@ class MentionTest < ActiveSupport::TestCase
   should belong_to(:entry).optional(true).inverse_of(:mentions)
   should belong_to(:mentioner).class_name("User")
   should belong_to(:mentionee).class_name("User")
+
+  test "url returns the profile path for the mentionee" do
+    mentionee = User.new(username: "john.doe")
+    mention = Mention.new(mentionee: mentionee)
+
+    assert_equal "/john.doe", mention.url
+  end
 end
