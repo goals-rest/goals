ce04244944fdab502abd2fdd42970c2e6a66ef67
Lucas Sousa <ls442336@gmail.com>
2025-03-29 11:34:08 -0300

    Remove validator for belong_to presence

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -2,7 +2,4 @@ class Mention < ApplicationRecord
   belongs_to :entry, optional: true, inverse_of: :mentions
   belongs_to :mentioner, class_name: "User"
   belongs_to :mentionee, class_name: "User"
-
-  validates :mentioner, presence: true
-  validates :mentionee, presence: true
 end
--- test/models/mention_test.rb
+++ test/models/mention_test.rb
@@ -4,7 +4,4 @@ class MentionTest < ActiveSupport::TestCase
   should belong_to(:entry).optional(true).inverse_of(:mentions)
   should belong_to(:mentioner).class_name("User")
   should belong_to(:mentionee).class_name("User")
-
-  should validate_presence_of(:mentioner)
-  should validate_presence_of(:mentionee)
 end
