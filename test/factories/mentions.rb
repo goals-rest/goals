ee3b9555d73caa88ec06d8301cc9e815de26e5f1
Lucas Sousa <ls442336@gmail.com>
2025-03-29 11:37:41 -0300

    Add mention factory associations

--- test/factories/mentions.rb
+++ test/factories/mentions.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :mention do
+    association :entry, factory: :entry
+    association :mentioner, factory: :user
+    association :mentionee, factory: :user
   end
 end
