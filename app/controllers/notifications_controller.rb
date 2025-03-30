ea8469e37835062d9a9f72f9a05f1f4fd972b3a3
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2025-03-30 08:39:02 -0300

    feat: Add notifications list and delete actions (#215)

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,15 @@
+class NotificationsController < DashboardController
+  def index
+    @notifications = Current.user.user_notifications.order(created_at: :desc)
+  end
+
+  def destroy
+    @notification = Current.user.user_notifications.find(params[:id])
+
+    if @notification.destroy
+      redirect_to notifications_path, notice: t(".success")
+    else
+      redirect_to notifications_path, alert: t(".error")
+    end
+  end
+end
