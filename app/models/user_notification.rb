6cadf4ca76b295913b75b92cfe57fe652fd4bc5a
Lucas Sousa <ls442336@gmail.com>
2025-04-13 11:00:28 -0300

    Move scope to module

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -4,28 +4,8 @@ class UserNotification < ApplicationRecord
 
   scope :unread, -> { where(read_at: nil) }
   scope :notification_center, -> do
-    includes(
-      notification: {
-        notifiable: [
-          entry_like: {
-            user: :avatar_attachment,
-            entry: [
-              :entryable
-            ]
-          },
-          entry: [
-            entryable: [
-              owner: :avatar_attachment,
-              parent: [
-                :entryable,
-                owner: :avatar_attachment
-              ]
-            ],
-            owner: :avatar_attachment
-          ]
-        ]
-      }
-    )
-    .order(created_at: :desc)
+    extending(UserNotificationScopes)
+      .with_full_notifiable_context
+      .order(created_at: :desc)
   end
 end
