fe433ba3fb034008e6d6c2f72b541b1adf7818b0
Lucas Sousa <ls442336@gmail.com>
2025-04-12 23:19:57 -0300

    feat: Reduce notifications queries

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -3,4 +3,29 @@ class UserNotification < ApplicationRecord
   belongs_to :notification
 
   scope :unread, -> { where(read_at: nil) }
+  scope :notification_center, -> do
+    includes(
+      notification: {
+        notifiable: [
+          entry_like: {
+            user: :avatar_attachment,
+            entry: [
+              :entryable
+            ]
+          },
+          entry: [
+            entryable: [
+              owner: :avatar_attachment,
+              parent: [
+                :entryable,
+                owner: :avatar_attachment
+              ]
+            ],
+            owner: :avatar_attachment
+          ]
+        ]
+      }
+    )
+    .order(created_at: :desc)
+  end
 end
