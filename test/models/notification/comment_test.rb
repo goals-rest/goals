e732c457f62cce4a60b85455eb4af38cbbe122f9
Karine Vieira <kv.vieira1@gmail.com>
2025-04-17 20:58:37 -0300

    fix: Update recipients method to correctly reference parent owner

--- test/models/notification/comment_test.rb
+++ test/models/notification/comment_test.rb
@@ -5,9 +5,9 @@ class Notification::CommentTest < ActiveSupport::TestCase
   should have_one(:notification)
 
   test "#recipients returns a list of recipients" do
-    comment = create(:entry, :comment)
-    notification_comment = create(:notification_comment, entry: comment)
-    recipient = comment.owner
+    entry_comment = create(:entry, :comment)
+    notification_comment = create(:notification_comment, entry: entry_comment)
+    recipient = entry_comment.comment.parent.owner
 
     assert_equal [ recipient ], notification_comment.recipients
   end
